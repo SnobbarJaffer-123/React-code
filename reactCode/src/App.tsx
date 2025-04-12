692dceb6965029e3e039daebbf86f5a160d61595
SnobbarJaffer-123 <snobbarjaffer2018@gmail.com>
2025-04-12 18:29:54 +0530

    new

--- reactCode/src/App.tsx
+++ reactCode/src/App.tsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 
 function App() {
+  let [counter,setCounter]=useState(0)
+  const addValue=()=>{
+    
+    console.log("clicked",counter)
+    if(counter<33){
+    setCounter(counter+1)
+    }
+  }
+
+  const decrementVal=()=>{
+    console.log("clicked",counter)
+    if(counter>0){
+    setCounter(counter-1)
+    }
+  }
 
     return (
-      <h1>snobbar jaffer </h1>
+      <>
+      <h1>snobbar jaffer :{counter}</h1>
+      <h3>counter between 0 and 33</h3>
+      <button onClick={addValue}>add value:{counter}</button>
+      
+      <button onClick={decrementVal}>decrement Value:{counter}</button>
+      </>
     );
 }
 
